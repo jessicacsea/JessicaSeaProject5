a9829f4040394e4b76ceb37ada5283d06529aa6a
Jessica <sea.jessicac@gmail.com>
2018-08-31 15:55:14 -0400

    DONE JAVASCRIPT I THINK

--- src/App.js
+++ src/App.js
@@ -14,27 +14,44 @@ class App extends Component {
         searchText: '',
         index: 0,
         result: [],
+        randomPics: [],
       }
   }
 
   userInputCall = (value) => {
     apiCall(value).then((res) => {
-      // once the user submits the form, it will save the searchText nto the new setState, use that variable to fill in the keyword param back at the api call (with the new user input as the variable) and come back here to return the list (we called res here) then we console.log it
+      // once the user submits the form, it will save the searchText nto the new setState, use that variable to fill in the keyword param back at the api call (with the new user input as the variable) and come back here to return the list (we called res here) then we console.log it\
       const result = res.data.collection.items;
-      // console.log(result);
+      console.log(result);
+      // this.setState ({
+      //   result: result,
+      // });
+      const groupOfPics = [];
+
+      for(let i=0; i < 5; i++){
+        const randomPic = this.RandomizePics(result);
+        groupOfPics.push(randomPic);
+      }
+      console.log(groupOfPics);
       this.setState ({
-        result: result,
-      });
+        randomPics: groupOfPics,
+      })
     })
   }
 
+  RandomizePics = (array) => {
+    const index = (Math.floor(Math.random() * array.length));
+    return array[index];
+  }
+  
+  
   render() {
+  
     return (
       <div className="App">
         <MainPage />
         <InputPage userInputCall={this.userInputCall} />
-        <ResultsPage newResult={this.state.result} />
-        {/* <ApiCall /> */}
+        <ResultsPage newResult={this.state.randomPics} />
       </div>
     );
   }
--- src/components/ResultsPage/ResultsPage.js
+++ src/components/ResultsPage/ResultsPage.js
@@ -1,41 +1,45 @@
 import React, { Component } from 'react';
 import App from '../../App';
-import InputPage from '../InputPage/InputPage.js';
-import apiCall from '../apiCall/apiCall.js';
 
 class ResultsPage extends Component {
   constructor (props) {
     super(props);
+    
   }
-  
+
   
   render() {
-    console.log('render', this.props.newResult.map((img) => {
-      return (
-        <li></li>
-      )
+    console.log(this.props.newResult)
+    this.props.newResult.map((img) => {
+      console.log(img.links[0].href);
     })
-    return( // WHAT IS HAPPENING HERE?! WHY CANT I CALL THE OBJECT ARRAY OBJECT
+
+    return (
       <div className="resultsPage">
         <h2>Houston... we have some photos</h2>
         <h3>Keywords: </h3>
         <ul>
-      
+          {this.props.newResult.map((img) => {
+            return (
             <li>
-            <img src={'this.props.newResult[0].links[0].href'} />
+              <img src={img.links[0].href} />
             </li>
+            )})}
         </ul>
 
-      </div> 
+      </div>
+    
     )}
   }
-  // get something on the page!!!!
   // now that the results are on the apiCall.js page you can access it and prop it and pull it back down this result page
 
 
 export default ResultsPage;
 
 
-// want to pull the array from the api call that we created from the input
+// want to pull the array from the api call that we created from the input -- COMPLETE
 // grab 5 random images to display
 // add a click event to scale bigger and show description and image title
+
+// move randomizer in app.js so that you randomize the photo first before sending it as a prop to results page
+// use whats next in JS to use the map and set method to pull 5 randoms pictures into an array but not displaying the same photo twice
